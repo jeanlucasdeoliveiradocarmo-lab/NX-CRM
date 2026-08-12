a243541d2cd8f36003db28ce44f7d4fac6ec6635
jeanlucasdeoliveiradocarmo-lab <jeanlucasdeoliveiradocarmo@gmail.com>
2026-08-12 14:24:11 -0300

    Add dynamic rendering and revalidation settings

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import { useEffect, useMemo, useState } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import {
