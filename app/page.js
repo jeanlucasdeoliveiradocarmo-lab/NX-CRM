834964c4ba3c34e2b56b3b51330dca9b802d470b
jeanlucasdeoliveiradocarmo-lab <jeanlucasdeoliveiradocarmo@gmail.com>
2026-08-12 14:40:16 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,5 @@
 'use client';
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
 import { useEffect, useMemo, useState } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import {
